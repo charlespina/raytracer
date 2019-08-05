9e680fabc977d2741497dca4637044c1ff3ee08d
Charles Pina <cpina@adobe.com>
2019-08-04 22:26:54 -0700

    mesh in progress

--- include/raytracer/Mesh.h
+++ include/raytracer/Mesh.h
@@ -6,15 +6,22 @@
 #include "raytracer/Plane.h"
 #include "raytracer/Vec3.h"
 
+namespace raytracer {
+
 class Mesh : public IHitable {
 public:
   Mesh() = delete;
-  Mesh(std::vector<Vec3> vertex_triplets);
+  Mesh(const std::vector<Vec3> &vertex_triplets);
   virtual ~Mesh() {}
+
+  virtual bool hit(const Ray& r, float t_min, float t_max, HitRecord &record) const override;
+  virtual bool bounding_box(float t0, float t1, AxisAlignedBoundingBox& aabb) const override;
 
 public:
   std::vector<Vec3> _vertices;
   std::vector<size_t> _triangle_indices;
 };
+
+}
 
 #endif
--- src/Mesh.cpp
+++ src/Mesh.cpp
@@ -1,10 +1,26 @@
 #include "raytracer/Mesh.h"
 #include <cassert>
 
-Mesh::Mesh(std::vector<Vec3> vertex_triplets) : _vertices(vertex_triplets) {
+namespace raytracer {
+
+Mesh::Mesh(const std::vector<Vec3> &vertex_triplets) : _vertices(vertex_triplets) {
   assert(vertex_triplets.size() % 3 == 0);
   _triangle_indices.resize(vertex_triplets.size());
   for (size_t vi=0; vi<vertex_triplets.size(); vi++) {
    _triangle_indices[vi] = vi; 
   }
+}
+
+bool Mesh::hit(const Ray& r, float t_min, float t_max, HitRecord &record) const {
+  // todo!
+  return false;
+}
+
+bool Mesh::bounding_box(float t0, float t1, AxisAlignedBoundingBox& aabb) const {
+  for (const auto &v : _vertices) {
+    aabb.extend(v);
+  }
+  return true;
+}
+
 }
