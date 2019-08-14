9540b2debbcc678bfff87d34ec8542c3f7daf178
Charles Pina <cpina@adobe.com>
2019-08-14 16:35:30 -0700

    medium in progress (really 2)

--- src/objects/ConstantMedium.cpp
+++ src/objects/ConstantMedium.cpp
@@ -11,8 +11,25 @@ ConstantMedium::ConstantMedium(std::shared_ptr<IObject> object, float density, s
 }
 
 bool ConstantMedium::hit(const Ray& r, float t_min, float t_max, HitRecord &record) const {
+  HitRecord hita, hitb;
+  if (!boundary->hit(r, std::numeric_limits<float>::lowest(), std::numeric_limits<float>::max(), hita))
+    return false;
+  if (!boundary->hit(r, hita.t + 0.0001f, std::numeric_limits<float>::max(), hitb))
+    return false;
+  
+  if (hita.t < t_min)
+    hita.t = t_min;
+  if (hitb.t > t_max)
+    hitb.t = t_max;
+  if (hita.t >= hitb.t)
+    return false;
+  if (hita.t < 0)
+    hita.t = 0;
+  
+  float distance_inside_boundary =
 
 }
+
 bool ConstantMedium::bounding_box(float t0, float t1, AxisAlignedBoundingBox& aabb) const {
   return _object->bounding_box(t0, t1, aabb);
 }
